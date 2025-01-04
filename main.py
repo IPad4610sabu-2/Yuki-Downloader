819646c41b824e0eb0a843eec7b2f2c61afe3d38
IPad4610sabu-2 <aihfhauu@gmail.com>
2025-01-04 19:32:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,145 +1,69 @@
-import json
-import requests
-import urllib.parse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-import datetime
-import random
 import os
-import subprocess
-from cache import cache
 
 
-max_api_wait_time = 3
-max_time = 10
-apis = [r"https://iv.datura.network/",r"https://invidious.private.coffee/",r"https://invidious.protokolla.fi/",r"https://invidious.perennialte.ch/",r"https://yt.cdaut.de/",r"https://invidious.materialio.us/",r"https://yewtu.be/",r"https://invidious.fdn.fr/",r"https://inv.tux.pizza/",r"https://invidious.privacyredirect.com/",r"https://invidious.drgns.space/",r"https://vid.puffyan.us",r"https://invidious.jing.rocks/",r"https://youtube.076.ne.jp/",r"https://vid.puffyan.us/",r"https://inv.riverside.rocks/",r"https://invidio.xamh.de/",r"https://y.com.sb/",r"https://invidious.sethforprivacy.com/",r"https://invidious.tiekoetter.com/",r"https://inv.bp.projectsegfau.lt/",r"https://inv.vern.cc/",r"https://invidious.nerdvpn.de/",r"https://inv.privacy.com.de/",r"https://invidious.rhyshl.live/",r"https://invidious.slipfox.xyz/",r"https://invidious.weblibre.org/",r"https://invidious.namazso.eu/",r"https://invidious.jing.rocks"]
-url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
-version = "1.0"
+def get_mp3_downloads(video_url):
+    options = webdriver.ChromeOptions()
+    options.add_argument("user-data-dir=C:\\Users\\dell\\SeleniumProjects\\automation_profile")  # Your Selenium profile path
+    prefs = {"download.default_directory": "D:\\Downloads"}  # Your desired download path
+    options.add_experimental_option("prefs", prefs)
+    options = webdriver.ChromeOptions()  # Use EdgeOptions() for Edge
+    driver = webdriver.Chrome(options=options) 
+    driver.get(video_url)
 
-os.system("chmod 777 ./yukiverify")
+    try:       
+        # Handle initial redirection ads (Repeated Attempts)
+        for _ in range(5): 
+            time.sleep(2) 
+            potential_ad_elements = driver.find_elements(By.TAG_NAME, "iframe") 
+            for element in potential_ad_elements:
+                try:
+                    driver.switch_to.frame(element)
+                    close_button = driver.find_element(By.CSS_SELECTOR, "button.close")  # Adjust selector
+                    close_button.click()
+                except:
+                    pass  
+                finally:
+                    driver.switch_to.default_content()  
+                
 
-apichannels = []
-apicomments = []
-[[apichannels.append(i),apicomments.append(i)] for i in apis]
-class APItimeoutError(Exception):
-    pass
+        # Click the "Mp3" button 
+        mp3_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "Mp3")) 
+        )
+        mp3_button.click()
 
-def is_json(json_str):
-    result = False
-    try:
-        json.loads(json_str)
-        result = True
-    except json.JSONDecodeError as jde:
-        pass
-    return result
+        # Find the last download button with the desired attributes
+        all_download_buttons = driver.find_elements(By.CLASS_NAME, "btn-download-size")
+        last_button = None
+        for button in all_download_buttons:
+            if button.get_attribute("data-ftype") == "mp3" and button.get_attribute("data-fquality") == "256":
+                last_button = button
+                break
 
-def apirequest(url):
-    global apis
-    global max_time
-    starttime = time.time()
-    for api in apis:
-        if  time.time() - starttime >= max_time -1:
-            break
-        try:
-            res = requests.get(api+url,timeout=max_api_wait_time)
-            if res.status_code == 200 and is_json(res.text):
-                return res.text
-            else:
-                print(f"エラー:{api}")
-                apis.append(api)
-                apis.remove(api)
-        except:
-            print(f"タイムアウト:{api}")
-            apis.append(api)
-            apis.remove(api)
-    raise APItimeoutError("APIがタイムアウトしました")
-
-def apichannelrequest(url):
-    global apichannels
-    global max_time
-    starttime = time.time()
-    for api in apichannels:
-        if  time.time() - starttime >= max_time -1:
-            break
-        try:
-            res = requests.get(api+url,timeout=max_api_wait_time)
-            if res.status_code == 200 and is_json(res.text):
-                return res.text
-            else:
-                print(f"エラー:{api}")
-                apichannels.append(api)
-                apichannels.remove(api)
-        except:
-            print(f"タイムアウト:{api}")
-            apichannels.append(api)
-            apichannels.remove(api)
-    raise APItimeoutError("APIがタイムアウトしました")
-
-def apicommentsrequest(url):
-    global apicomments
-    global max_time
-    starttime = time.time()
-    for api in apicomments:
-        if  time.time() - starttime >= max_time -1:
-            break
-        try:
-            res = requests.get(api+url,timeout=max_api_wait_time)
-            if res.status_code == 200 and is_json(res.text):
-                return res.text
-            else:
-                print(f"エラー:{api}")
-                apicomments.append(api)
-                apicomments.remove(api)
-        except:
-            print(f"タイムアウト:{api}")
-            apicomments.append(api)
-            apicomments.remove(api)
-    raise APItimeoutError("APIがタイムアウトしました")
-
-
-def get_info(request):
-    global version
-    return json.dumps([version,os.environ.get('RENDER_EXTERNAL_URL'),str(request.scope["headers"]),str(request.scope['router'])[39:-2]])
-
-def get_data(videoid):
-    global logs
-    t = json.loads(apirequest(r"api/v1/videos/"+ urllib.parse.quote(videoid)))
-    return [{"id":i["videoId"],"title":i["title"],"authorId":i["authorId"],"author":i["author"]} for i in t["recommendedVideos"]],list(reversed([i["url"] for i in t["formatStreams"]]))[:2],t["descriptionHtml"].replace("\n","<br>"),t["title"],t["authorId"],t["author"],t["authorThumbnails"][-1]["url"]
-
-def get_search(q,page):
-    global logs
-    t = json.loads(apirequest(fr"api/v1/search?q={urllib.parse.quote(q)}&page={page}&hl=jp"))
-    def load_search(i):
-        if i["type"] == "video":
-            return {"title":i["title"],"id":i["videoId"],"authorId":i["authorId"],"author":i["author"],"length":str(datetime.timedelta(seconds=i["lengthSeconds"])),"published":i["publishedText"],"type":"video"}
-        elif i["type"] == "playlist":
-            return {"title":i["title"],"id":i["playlistId"],"thumbnail":i["videos"][0]["videoId"],"count":i["videoCount"],"type":"playlist"}
+        if last_button:
+            last_button.click()
+            time.sleep(3)  # Optional pause after clicking if needed
         else:
-            if i["authorThumbnails"][-1]["url"].startswith("https"):
-                return {"author":i["author"],"id":i["authorId"],"thumbnail":i["authorThumbnails"][-1]["url"],"type":"channel"}
-            else:
-                return {"author":i["author"],"id":i["authorId"],"thumbnail":r"https://"+i["authorThumbnails"][-1]["url"],"type":"channel"}
-    return [load_search(i) for i in t]
+            print("Download button with specified attributes not found.")
 
-def get_channel(channelid):
-    global apichannels
-    t = json.loads(apichannelrequest(r"api/v1/channels/"+ urllib.parse.quote(channelid)))
-    if t["latestVideos"] == []:
-        print("APIがチャンネルを返しませんでした")
-        apichannels.append(apichannels[0])
-        apichannels.remove(apichannels[0])
-        raise APItimeoutError("APIがチャンネルを返しませんでした")
-    return [[{"title":i["title"],"id":i["videoId"],"authorId":t["authorId"],"author":t["author"],"published":i["publishedText"],"type":"video"} for i in t["latestVideos"]],{"channelname":t["author"],"channelicon":t["authorThumbnails"][-1]["url"],"channelprofile":t["descriptionHtml"]}]
 
-def get_playlist(listid,page):
-    t = json.loads(apirequest(r"/api/v1/playlists/"+ urllib.parse.quote(listid)+"?page="+urllib.parse.quote(page)))["videos"]
-    return [{"title":i["title"],"id":i["videoId"],"authorId":i["authorId"],"author":i["author"],"type":"video"} for i in t]
+        button = driver.find_element(By.CSS_SELECTOR, "a.btn.btn-success.btn-download-link")
+        driver.execute_script("arguments[0].click();", button)
 
-def get_comments(videoid):
-    t = json.loads(apicommentsrequest(r"api/v1/comments/"+ urllib.parse.quote(videoid)+"?hl=jp"))["comments"]
-    return [{"author":i["author"],"authoricon":i["authorThumbnails"][-1]["url"],"authorid":i["authorId"],"body":i["contentHtml"].replace("\n","<br>")} for i in t]
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        time.sleep(10) 
+        driver.quit()
 
-def get_replies(videoid,key):
-    t = json.loads(apicommentsrequest(fr"api/v1/comments/{videoid}?hmac_key={key}&hl=jp&format=html"))["contentHtml"]
+# Example usage
+video_url = "https://y2meta.app/en/youtube/{q}"  
+get_mp3_downloads(videoid)
 
 def get_level(word):
     for i1 in range(1,13):
@@ -216,20 +140,6 @@ def search(q:str,response: Response,request: Request,page:Union[int,None]=1,yuki
     response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
     return template("search.html", {"request": request,"results":get_search(q,page),"word":q,"next":f"/search?q={q}&page={page + 1}","proxy":proxy})
 
-@app.get("/hashtag/{tag}")
-def search(tag:str,response: Response,request: Request,page:Union[int,None]=1,yuki: Union[str] = Cookie(None)):
-    if not(check_cokie(yuki)):
-        return redirect("/")
-    return redirect(f"/search?q={tag}")
-
-
-@app.get("/channel/{channelid}", response_class=HTMLResponse)
-def channel(channelid:str,response: Response,request: Request,yuki: Union[str] = Cookie(None),proxy: Union[str] = Cookie(None)):
-    if not(check_cokie(yuki)):
-        return redirect("/")
-    response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
-    t = get_channel(channelid)
-    return template("channel.html", {"request": request,"results":t[0],"channelname":t[1]["channelname"],"channelicon":t[1]["channelicon"],"channelprofile":t[1]["channelprofile"],"proxy":proxy})
 
 @app.get("/answer", response_class=HTMLResponse)
 def set_cokie(q:str):
@@ -240,39 +150,16 @@ def set_cokie(q:str):
         return "level12以上\nほぼ推測必須"
     return f"level{t}\n覚えておきたいレベル"
 
-@app.get("/playlist", response_class=HTMLResponse)
-def playlist(list:str,response: Response,request: Request,page:Union[int,None]=1,yuki: Union[str] = Cookie(None),proxy: Union[str] = Cookie(None)):
-    if not(check_cokie(yuki)):
-        return redirect("/")
-    response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
-    return template("search.html", {"request": request,"results":get_playlist(list,str(page)),"word":"","next":f"/playlist?list={list}","proxy":proxy})
-
-@app.get("/info", response_class=HTMLResponse)
-def viewlist(response: Response,request: Request,yuki: Union[str] = Cookie(None)):
-    global apis,apichannels,apicomments
-    if not(check_cokie(yuki)):
-        return redirect("/")
-    response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
-    return template("info.html",{"request": request,"Youtube_API":apis[0],"Channel_API":apichannels[0],"Comments_API":apicomments[0]})
 
 @app.get("/suggest")
 def suggest(keyword:str):
     return [i[0] for i in json.loads(requests.get(r"http://www.google.com/complete/search?client=youtube&hl=ja&ds=yt&q="+urllib.parse.quote(keyword)).text[19:-1])[1]]
 
-@app.get("/comments")
-def comments(request: Request,v:str):
-    return template("comments.html",{"request": request,"comments":get_comments(v)})
 
 @app.get("/thumbnail")
 def thumbnail(v:str):
     return Response(content = requests.get(fr"https://img.youtube.com/vi/{v}/0.jpg").content,media_type=r"image/jpeg")
 
-@app.get("/bbs",response_class=HTMLResponse)
-def view_bbs(request: Request,name: Union[str, None] = "",seed:Union[str,None]="",channel:Union[str,None]="main",verify:Union[str,None]="false",yuki: Union[str] = Cookie(None)):
-    if not(check_cokie(yuki)):
-        return redirect("/")
-    res = HTMLResponse(requests.get(fr"{url}bbs?name={urllib.parse.quote(name)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}",cookies={"yuki":"True"}).text)
-    return res
 
 @cache(seconds=5)
 def bbsapi_cached(verify,channel):
@@ -302,16 +189,3 @@ def view_commonds(request: Request,yuki: Union[str] = Cookie(None)):
         return redirect("/")
     return how_cached()
 
-@app.get("/load_instance")
-def home():
-    global url
-    url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
-
-
-@app.exception_handler(500)
-def page(request: Request,__):
-    return template("APIwait.html",{"request": request},status_code=500)
-
-@app.exception_handler(APItimeoutError)
-def APIwait(request: Request,exception: APItimeoutError):
-    return template("APIwait.html",{"request": request},status_code=500)
